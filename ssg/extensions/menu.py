7cc7bb3014e4fe907ecd570b04d11cdcef1744de
thegame724 <sdipasquale@outlook.com>
2023-03-15 15:29:34 -0400

    Fixing errors in module 3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,7 +4,7 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p : not p.isinstance(parsers.ResourceParser)
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
@@ -12,7 +12,7 @@ def collect_files(source, site_parsers):
 
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
-    template = r'<li><a href="{}{}">{}</a></li>'
+    template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext : template.format(name, ext, name.title())
     menu = "\n".join([ menu_item(path.stem, ext) for path in files ])
     return "<ul>\n{}<ul>\n{}".format(menu, html)
